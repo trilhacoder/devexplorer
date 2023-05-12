e885dc222818c6f7c4a1e93607ecbfe9823830b5
TrilhaFront <contato@trilhafront.com.br>
2023-05-12 16:29:30 +0000

    video-08 deploy

--- video-08/backend/api.js
+++ video-08/backend/api.js
@@ -7,7 +7,7 @@ let app = express()
 app.use(cors())
 app.use(express.json())
 
-let bd = pgp()(process.env.DATABASE_URL)
+let bd = pgp()("postgres://postgres:postgres@localhost:5432/postgres")
 
 app.get("/tarefas", async function(req, res) {
     let tarefas = await bd.query("select * from tarefas")
@@ -29,6 +29,6 @@ app.put("/tarefas/:id", async function(req, res) {
     res.end()
 })
 
-app.listen(process.env.PORT, function() {
-    console.log(`API REST executando na porta ${process.env.PORT} ...`)
+app.listen(3100, function() {
+    console.log("API REST executando na porta 3100 ...")
 })
